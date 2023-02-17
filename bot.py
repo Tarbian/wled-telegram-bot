f5e2e9fda8655df7ff1eb66294c5a6aaba0ff959
Tarbian <anyfuture@protonmail.com>
2023-02-17 15:01:00 +0200

    Improvement 1

--- bot.py
+++ bot.py
@@ -1,36 +1,36 @@
 from config import *
 from requests import get
+from os.path import exists
 from telebot import TeleBot
 from json import dump, load
 from re import compile, match
+from json.decoder import JSONDecodeError
 from telebot.types import ReplyKeyboardMarkup
 from requests.exceptions import ConnectionError
 from xml.etree.ElementTree import ElementTree, fromstring
 
 bot = TeleBot(bot_token)
-
-try:
-    with open("wled_ip.json") as f:
-        wled_url = "http://" + load(f) + "/win"
-except FileNotFoundError:
-    wled_url = "http://" + wled_ip + "/win"
-    with open("wled_ip.json", "w") as f:
-            dump(wled_ip, f)
-
-try:
-    with open("presets.json") as f:
-        presets = load(f)
-except FileNotFoundError:
-    with open("presets.json", "w") as f:
-            dump(presets, f)
+new_config = "config.json"
+if exists(new_config):
+    with open(new_config, "r") as f:
+        config = load(f)
+    wled_ip = config["wled_ip"]
+    presets = config["presets"]
+else:
+    config = {
+        "wled_ip": wled_ip,
+        "presets": presets}
+    with open(new_config, "w") as f:
+        dump(config, f)
+wled_url = f"http://{wled_ip}/win"
 
 def chose_action(button):
     global presets
     global white_id
     return {
-        '◀': "&P1=1&P2=" + presets + "&PL=~-",
-        '⚪': "&PL=" + white_id,
-        '▶': "&P1=1&P2=" + presets + "&PL=~",
+        '◀': f"&P1=1&P2={presets}&PL=~-",
+        '⚪': f"&PL={white_id}",
+        '▶': f"&P1=1&P2={presets}&PL=~",
         '◐': "&T=2",
         '☀': "&A=255",
         '🔅': "&A=~-30",
@@ -45,22 +45,23 @@ def set_ip(message):
     wled_ip = message.text
     global wled_url
     if is_valid_ip(wled_ip) == True:
-        wled_url = "http://" + message.text + "/win"
-        with open("wled_ip.json", "w") as f:
-            dump(wled_ip, f)
-        response = "WLED ip set to " + message.text
+        wled_url = f"http://{message.text}/win"
+        config["wled_ip"] = message.text
+        with open(new_config, "w") as f:
+            dump(config, f)
+        response = f"WLED ip set to {message.text}"
     else:
         response = "Wrong ip format!"
     bot.reply_to(message, response)
 
-def set_bright(message):
+def set_brightness(message):
     try:
         value = abs(int(float(message.text)))
         if value >= 255:
             value = 255
         action =  '&A=' + str(value)
-        get(wled_url + action, timeout=10, verify=False)
-        bot.reply_to(message, "Brightness set to " + str(value))
+        get_wled_response(action)
+        bot.reply_to(message, f"Brightness set to {value}")
     except:
         bot.reply_to(message, "Something wrong! Write a natural number between 0 and 255")
 
@@ -69,12 +70,39 @@ def set_presets(message):
         value = abs(int(float(message.text)))
         if value <= 255 and value >= 0:
             global presets 
-            presets = message.text
-            with open("presets.json", "w") as f:
-                dump(presets, f)
-            bot.reply_to(message, "Max preset index set to " + presets)
+            presets = value
+            config["presets"] = message.text
+            with open(new_config, "w") as f:
+                dump(config, f)
+            bot.reply_to(message, f"Max preset index set to {presets}")
     except:
         bot.reply_to(message, "Something wrong! Write a natural number between 0 and 255")
+
+def get_wled_response(action):
+    root = ElementTree(fromstring(get(wled_url + action, timeout=10,
+        verify=False).content)).getroot()
+    return get_info()
+
+def format_message_response(response, task):
+    brightness = response['brightness']
+    info = 'Brightness ' + brightness
+    if task ==  '◐' and brightness == '0':
+        state = 'off❌'
+    elif task ==  '◐' and brightness != '0':
+        state = 'on✅'
+    else:
+        state = '🆗'
+        info = get_info()['full']
+    return f"{state}\n{info}"
+
+def get_info():
+    root = ElementTree(fromstring(get(wled_url,
+        timeout=10, verify=False).content)).getroot()
+    brightness = root[0].text
+    presets = root[19].text
+    return {'brightness': brightness,
+                'presets': presets,
+                  'full': f'Brightness {brightness}\nPreset {presets}'}
  
 @bot.message_handler(func=lambda message: message.chat.id in white_list, commands=['start'])
 def start(message):
@@ -100,32 +128,19 @@ def correct_presets(message):
 @bot.message_handler(func=lambda message: message.chat.id in white_list, commands=['status'])
 def status(message):
     try:
-        root = ElementTree(fromstring(get(wled_url,
-            timeout=10, verify=False).content)).getroot()
-        bright = root[0].text
-        preset = str(int(root[19].text) + 1)
-        response = 'Brightness ' + bright + '\nPreset ' + preset
+        response = get_info()['full']
         bot.reply_to(message, response)
     except ConnectionError as e:
         bot.reply_to(message, "ERROR\nIp address is not reachable")
         print(e)
-    
+
 @bot.message_handler(func=lambda message: message.chat.id in white_list)
-def switch(message): 
+def handle_message(message):
     task = message.text
     if task in ['◀', '⚪', '▶', '◐', '☀', '🔅', '🔆']:
         action = chose_action(task)
         try:
-            root = ElementTree(fromstring(get(wled_url + action,
-                timeout=10, verify=False).content)).getroot()
-            bright = root[0].text
-            preset = str(int(root[19].text) + 1)
-            if task == '◐' and bright == '0':
-                response = 'off❌' + '\nBrightness ' + bright + '\nPreset ' + preset
-            elif task == '◐' and bright != '0':
-                response = 'on✅' + '\nBrightness ' + bright + '\nPreset ' + preset
-            else:
-                response = task + '\nBrightness ' + bright + '\nPreset ' + preset
+            response = format_message_response(get_wled_response(action), task)
             bot.reply_to(message, response)
         except ConnectionError as e:
             bot.reply_to(message, "ERROR\nIp address is not reachable")
@@ -133,8 +148,9 @@ def switch(message):
         except IndexError as e:
             bot.reply_to(message, "ERROR\nWrong ip address (IndexError)")
             print(e)
-        except:
-            bot.reply_to(message, "ERROR\nIdk what is wrong")
+        except Exception as e:
+            bot.reply_to(message, "ERROR\nUnknown error occurred")
+            print(e)
     elif task == '🌓':
         markup = ReplyKeyboardMarkup(one_time_keyboard=False)
         markup.add('🔅', '✍', '🔆', '←', '◐', '☀')
@@ -142,10 +158,10 @@ def switch(message):
     elif task == '←':
         markup = ReplyKeyboardMarkup(one_time_keyboard=False)
         markup.add('◀', '⚪', '▶', '🌓', '◐')
-        bot.reply_to(message, '⚙Main control pannel⚙', reply_markup=markup)
+        bot.reply_to(message, '⚙Main control panel⚙', reply_markup=markup)
     elif task == '✍':
         bot.reply_to(message, "Write a brightness (0..255)")
-        bot.register_next_step_handler(message, set_bright)
+        bot.register_next_step_handler(message, set_brightness)
     else:
         pass
 
